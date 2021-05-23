c9de013ef42c3c3aeeb98be3ec0ff4994ef5d7d8
jeroen dijkmeijer <jeroen@stretchsketch.com>
2021-05-23 21:23:54 +0200

    focus displays color of finger

--- src/Course.svelte
+++ src/Course.svelte
@@ -5,13 +5,20 @@
   import ODO from "./ODO.svelte";
   export let theCourse;
   export let courseText;
+  let char2Class = {};
   let wordText = "";
   let wordList = [];
   let theLayout = [];
   
   fetch("layouts/" + theCourse.layout + ".json").then(
-    (resp) => (theLayout = resp.json())
-  );
+    (resp) => { theLayout = resp.json();
+      theLayout.then(layout => layout.map(row => row.map(key => 
+        key.keys.filter(label => label).map(label => {
+          if (label.match(/[A-Z]/)) {
+            char2Class[label.toLowerCase()] = `${key["class"]} ${key.parentClass}`
+          }
+          if (label !== ' ') char2Class[label] = `${key["class"]} ${key.parentClass}`})))
+    )});
 </script>
 
 <div class="course">
@@ -29,7 +36,7 @@
     <div class="type input text">
       <div class="gauge"><Gauge speed={$velocity/8} errorrate={$errors / $totalHits}/>
         <div class="odo"><ODO value={courseText.length}/></div></div> 
-        <span class="focus">{@html courseText[0] === " " ? "&nbsp;" : courseText[0]}</span>{courseText.substring(1)}
+        <div class="focus {char2Class[courseText[0]]}">{@html courseText[0] === " " ? "&nbsp;" : courseText[0]}</div>{courseText.substring(1)}
     </div>
     {#each rows as row}
       <Row rowData={row}/>
@@ -45,14 +52,21 @@
     font-size: 0.7em;
   }
   .focus {
+      width: 0.9em;
       padding-bottom: -0.05em;
-      background-color: #CC44BB;
-      border-bottom: 2px solid #CC4444;
+      border: 1px solid black;
+      display: inline-block;
   }
   .course {
     width: 85%;
     font-family: TypeWriter;
     overflow: hidden
+  }
+  .focus.rh {
+    border-right: 3px solid #CC44BB;
+  }
+  .focus.lh {
+    border-left: 3px solid #CC44BB;
   }
   .course .description {
     font-size: 1.9em;
@@ -82,4 +96,21 @@
     border-radius: 6pt;
     overflow: hidden;
   }
+
+  .focus.pink {
+        background-color: #ffb6c1;
+    }
+    .focus.rf {
+        background-color: #eee8aa
+    }
+    .focus.mf {
+        background-color: #e7d0b0;
+    }
+    .focus.if {
+        background-color: #7fffd4;
+    }
+    .focus.thum {
+        background-color: #a9a9a9;
+    }
+    
 </style>
